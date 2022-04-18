835a282454127312d019720df54c236efa5d509f
Lexvg93 <alejandro_gonzalo92@icloud.com>
2022-04-18 12:15:25 +0200

    agregamos modificacion js

--- destructura.js
+++ destructura.js
@@ -1,2 +1,5 @@
 /*apuntes de curso online*/ 
+/* Destructuring es un modo de extraer valores de datos almacenados en objetos y arrays
+descomponiendolos y asignandolos a un grupo de variables.
+Mediante patrones se pueden especificar  */
 const galicia = ["A Coruña","Lugo","Ourense","Pontevedra"];
