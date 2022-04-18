54a3e7ddd65c50753bff0795add83587ee246651
Lexvg93 <alejandro_gonzalo92@icloud.com>
2022-04-18 12:18:21 +0200

    agregamos modificacion js

--- destructura.js
+++ destructura.js
@@ -3,3 +3,5 @@
 descomponiendolos y asignandolos a un grupo de variables.
 Mediante patrones se pueden especificar  */
 const galicia = ["A Coruña","Lugo","Ourense","Pontevedra"];
+const [c, lu, ou] = galicia;
+console.log
